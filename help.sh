b7fa1ce3d783ec2c1327c7c835d8a352fb988d6a
Alexis Crozet <alexis.crozet@epitech.eu>
2020-08-21 12:50:50 +0200

    ADD: help command script

---
+++ help.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source "$OMEGA_ROOT"/.omega-cfg
+source "$OMEGA_ROOT"/error_handling.sh
+source "$OMEGA_ROOT"/toolbox.sh
+
+omega_speach "Okay here's the list of the §pwords§n I understand :"
+
+omega_raw "§bnew§n§p: Create a new repository\n"
+omega_raw "§bhelp/?§n§p: Display this menu\n"
+omega_endlog
